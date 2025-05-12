5e8bde30d6beb77a3c3e42f4960e933407f5a4ec
caio <caio@guiafacil.com>
2025-05-12 17:07:48 -0300

    estudos-aula40-secao6-I

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
 
-Route::get('/sobremim', 'SobreMimController@sobreMim');
+Route::get('/sobremim', 'SobreMimController@sobreMim')->name('site.sobremim');
 
-Route::get('/contato', 'ContatoController@contato');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
 
 Route::get('/contato/{nome?}/{categoria_id?}/{assnto?}/{mensagem?}', 
 function(
@@ -34,22 +34,22 @@ function(
 
 Route::get('/login', function(){
     return 'Login';
-});
+})->name('site.login');
 
 
 Route::prefix('/app')->group(function(){
 
         Route::get('/clientes', function(){
         return 'Clientes';
-    });
+    })->name('app.clientes');
 
     Route::get('/fornecedores', function(){
         return 'Fornecedores';
-    });
+    })->name('app.fornecedores');
 
     Route::get('/produtos', function(){
         return 'Produtos';
-    });
+    })->name('app.produtos');
 
 });
 
