d95d8824f54c93f6841f26ad577342e1a8bc729d
caio <caio@guiafacil.com>
2025-05-13 13:54:38 -0300

    estudos-aula43-secao7-F--aula44-secao7-F--aula45-secao7-I

--- routes/web.php
+++ routes/web.php
@@ -43,9 +43,7 @@ Route::prefix('/app')->group(function(){
         return 'Clientes';
     })->name('app.clientes');
 
-    Route::get('/fornecedores', function(){
-        return 'Fornecedores';
-    })->name('app.fornecedores');
+    Route::get('/fornecedores', 'ForncedorContorller@index')->name('app.fornecedores');
 
     Route::get('/produtos', function(){
         return 'Produtos';
