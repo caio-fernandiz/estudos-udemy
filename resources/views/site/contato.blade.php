4ceaf013d2ad9a1e38720e29288ffaff01422c43
caio <caio@guiafacil.com>
2025-06-16 16:16:28 -0300

    aula66-secao7-F--aula67-secao7-I

--- resources/views/site/contato.blade.php
+++ resources/views/site/contato.blade.php
@@ -3,20 +3,8 @@
 @section('titulo', $titulo)
 
 @section('conteudo')
-    <div class="topo">
-
-        <div class="logo">
-            <img src="{{ asset('img/logo.png') }}">
-        </div>
-
-        <div class="menu">
-            <ul>
-                <li><a href="{{ route('site.index') }}">Principal</a></li>
-                <li><a href="{{ route('site.sobremim') }}">Sobre Nós</a></li>
-                <li><a href="{{ route('site.contato') }}">Contato</a></li>
-            </ul>
-        </div>
-    </div>
+    
+@include('site.layouts._partials.topo')
 
     <div class="conteudo-pagina">
         <div class="titulo-pagina">
